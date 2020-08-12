2ca8eef5274e417aa57281064458acef244c786b
Reddeppa S <rsandhireddygari@gomoxie.com>
2020-08-12 11:54:53 +0530

    added email validation

--- script.js
+++ script.js
@@ -17,6 +17,11 @@ function showSuccess(input){
     formControl.className = 'form-control success';
 }
 
+function isValidEmail(email){
+    //verifies test@test.com
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 //Event listeners
 form.addEventListener('submit',function(e){
 e.preventDefault();
@@ -25,19 +30,23 @@ showError(username, 'username is required');
     }else{
         showSuccess(username);
     }
-if (email.value === '') {
-    showError(email, 'Email is required');
-} else {
-    showSuccess(email);
-}
-if (password.value === '') {
-    showError(password, 'password is required');
-} else {
-    showSuccess(password);
+    if (email.value === '') {
+        showError(email, 'Email is required');
     }
- if(password2.value === ''){
-showError(password2, 'password is required');
-    }else{
+    else if(!isValidEmail(email.value)){
+        showError(email, 'Email is not valid');
+    } else {
+        showSuccess(email);
+    }
+    if (password.value === '') {
+        showError(password, 'password is required');
+    } else {
+        showSuccess(password);
+        }
+    if(password2.value === ''){
+    showError(password2, 'password is required');
+        }
+    else{
         showSuccess(password2);
     }
 });
